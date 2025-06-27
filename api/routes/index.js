6952a6dd33f6d1a31488aacf42ee6d0996abf60c
manoj. <manoj.gori1993@gmail.com>
2025-06-27 16:08:56 +0530

    remove extra route

--- api/routes/index.js
+++ api/routes/index.js
@@ -7,7 +7,6 @@ import authRoutes from './authRoutes.js';
 
 
 const registerRoutes = (app) => {
-  app.use('/',authRoutes);
   app.use('/api/auth', authRoutes);
   app.use('/api/users', userRoutes);
   app.use('/api/admin', adminRoutes);
